41263b82c2b559ca88d00e335a32d68c62d66a5c
beikwx <1987627818@qq.com>
2019-07-31 23:15:58 +0800

    完成批量准备受体工具

--- Tools/batch_prepare_receptor.py
+++ Tools/batch_prepare_receptor.py
@@ -4,6 +4,7 @@
 """
 
 import os
+import os as os1
 import sys
 
 
@@ -14,26 +15,63 @@ def prepare_receptors(mgl_dir, receptors_path, output_dir):
     :param receptors_path: D:\\Desktop\\Receptors
     :param output_dir: D:\\Desktop\\output
     """
+    # 检查mgltools的路径
     mgl_python_path = mgl_dir + os.sep + "python.exe"
     if not os.path.exists(mgl_python_path):
         print("mgltools路径下没有python文件")
         sys.exit()
 
-    prepare_receptor4_path = mgl_dir + os.sep + "Lib" + os.sep + "site-packages" + \
-                             os.sep + "AutoDockTools" + os.sep + "Utilities24" + \
-                             os.sep + "prepare_receptor4"
+    prepare_receptor4_path = mgl_dir + os.sep + "Lib" + os.sep + "site-packages" \
+                             + os.sep + "AutoDockTools" + os.sep + "Utilities24" \
+                             + os.sep + "prepare_receptor4.py"
+
+    # print(prepare_receptor4_path)
     if not os.path.exists(prepare_receptor4_path):
-        print("没有发现准备prepare_receptor4.py文件")
+        print("没有发现prepare_receptor4.py文件")
         sys.exit()
 
-    receptors = os.listdir(receptors_path)
-    filter_receptors = []
-    output_files = []
-    for receptor in receptors:
-        if receptor.endswith(".pdb") or receptor.endswith(".mol2") or \
-                receptor.endswith(".pdbq"):
-            filter_receptors.append(receptor)
-            # 获得文件名
-            output_files.append(output_dir + os.sep + os.path.split(receptor)[1].split(".")[0] + ".pdbqt")
+    # 获取输入的文件名 xx.pdb
+    receptors_name = get_receptors(receptors_path)
 
     i = 0
+    while i < len(receptors_name):
+        input_file = receptors_path + os1.sep + receptors_name[i]
+        output_file = output_dir + os1.sep + receptors_name[i].split(".")[0] + ".pdbqt"
+        print("--------------------开始准备受体--------------------")
+        print("当前输入文件" + input_file)
+        cmd = "%s %s -r %s -o %s -A -e" % (mgl_python_path, prepare_receptor4_path, input_file, output_file)
+        # print(cmd)
+        os.system(cmd)
+        print("--------------------受体准备完成--------------------")
+        print("当前输出文件" + output_file)
+        i += 1
+
+
+def get_receptors(receptors_root_path):
+    receptors = os.listdir(receptors_root_path)
+    receptors_name = []
+    for receptor_path in receptors:
+        if receptor_path.endswith(".pdb") or receptor_path.endswith(".mol2") or \
+                receptor_path.endswith(".pdbq"):
+            receptors_name.append(os.path.split(receptor_path)[-1])
+    return receptors_name
+
+
+if __name__ == '__main__':
+    # 读取命令行输入
+    if len(sys.argv) == 1:
+        print("--------------------------------------------------------------------------------")
+        print('命令格式:\n'
+              'python .\\batch_prepare_receptor.py mgltools目录 受体目录 输出目录\n\n'
+              '其中:\n'
+              'mgltools目录：安装mgltools的目录，比如C:\\mgltools，如果路径包含空格请用引号，比如"C:\\Program files\\mgltools"\n'
+              '受体目录：所有受体所在的目录，比如D:\\Desktop\\Receptors\n'
+              '输出目录：准备后文件输出的目录，比如D:\\Desktop\\Proteins')
+        print("--------------------------------------------------------------------------------")
+        sys.exit()
+    elif len(sys.argv) != 4:
+        print("参数个数不正确，请检查参数！")
+        sys.exit()
+    else:
+        mgl_dir, receptors_path, output_dir = sys.argv[1:]
+        prepare_receptors(mgl_dir, receptors_path, output_dir)
